4b1d6b18fd34506449275281bb23cb0f178800db
Work <micnkru@gmail.com>
2020-09-10 07:42:48 +0000

    remove uncc files

--- src/modules/user/test/user.controller.test.js
+++
@@ -1,9 +0,0 @@
-import { nuke } from '../../../helpers/test_helpers';
-import User from '../user.controller';
-import server from '../../../server';
-
-describe('User::Controller', async () => {
-  it('user controller test', async () => {
-    expect(1 + 1).toBe(2);
-  });
-});
