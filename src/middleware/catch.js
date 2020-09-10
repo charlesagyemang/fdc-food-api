a19e1a0e95d5dea5eb64dc672ceb85b8cde0208e
Work <micnkru@gmail.com>
2020-09-10 05:30:58 +0000

    Init Setup with sequelize, postgres and a dummy user model which may or may not be needed but who knows

---
+++ src/middleware/catch.js
@@ -0,0 +1,20 @@
+import HTTPStatus from 'http-status';
+
+
+const errors = (app) => {
+  app.use((err, req, res, next) => {
+    res.status(err.status || HTTPStatus.INTERNAL_SERVER_ERROR)
+          .json({ ...err });
+    next();
+  });
+};
+
+const status404 = (app) => {
+  app.use((req, res) => {
+    res.status(404)
+      .json({ msg: `Cannot ${req.method} to ${req.url}` });
+  });
+};
+
+
+export { errors, status404 };
