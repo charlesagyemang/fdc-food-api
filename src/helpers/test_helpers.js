9967a0c3bfef63965d15cb3cebd993937d0cd5d5
Work <micnkru@gmail.com>
2020-09-11 05:42:57 +0000

    Add name to user helper

--- src/helpers/test_helpers.js
+++ src/helpers/test_helpers.js
@@ -4,7 +4,7 @@ import User from '../modules/user/user.model';
 
 // adding a comp parameter to solve async issues
 export const login = async (server) => {
-  const user = await User.create({ email: 'user@email.com', password: 'password' });
+  const user = await User.create({ name: 'user', email: 'user@email.com', password: 'password' });
   const res = await request(server).post('/api/user/login').send({
     email: user.email,
     password: user.password,
