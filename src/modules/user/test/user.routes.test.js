8bbd6dc42926f052544d3ccda0560d95a18b21a6
Work <micnkru@gmail.com>
2020-09-11 05:37:48 +0000

    Feat: Update user test

--- src/modules/user/test/user.routes.test.js
+++ src/modules/user/test/user.routes.test.js
@@ -6,10 +6,25 @@ import server from '../../../server';
 
 describe('User::Routes', async () => {
   beforeEach(async () => {
+    // BEFORE EACH TEST DELETE ALL STUFF IN THE DB
     await nuke();
   });
 
-  it.only('should login successfully', async () => {
+  it.only('should create user successfully', async () => {
+    const res = await request(server).post('/api/user/').send({
+      name: 'Kofi',
+      email: 'test@email.com',
+      password: 'password',
+      other: {
+        role: 'user',
+      },
+    });
+    expect(res.statusCode).toBe(HTTPStatus.CREATED);
+    expect(res.body).toHaveProperty('id');
+    expect(res.body).toHaveProperty('token');
+  });
+
+  it.skip('should login successfully', async () => {
     await User.create({
       name: 'Kofi',
       email: 'test@email.com',
--- src/modules/user/user.validation.js
+++ src/modules/user/user.validation.js
@@ -4,17 +4,15 @@ export default {
   register: {
     body: {
       name: Joi.string().required(),
-      eamil: Joi.string().email().required(),
+      email: Joi.string().email().required(),
       password: Joi.string().min(6).max(30).required(),
       other: Joi.object(),
     },
   },
   login: {
     body: {
-      name: Joi.string(),
       email: Joi.string().email().required(),
       password: Joi.string().min(6).max(60).required(),
-      other: Joi.object(),
     },
   },
 };
